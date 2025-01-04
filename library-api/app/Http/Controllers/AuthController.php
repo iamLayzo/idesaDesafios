d4dce6ed8c7dfaad56bab212b86a41965ee41be6
jesusb <jesusbclock@gmail.com>
2025-01-04 18:27:21 -0300

    Add Auth Controller

---
+++ library-api/app/Http/Controllers/AuthController.php
@@ -0,0 +1,48 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
+        ]);
+
+        $user = User::create([
+            'name' => $validated['name'],
+            'email' => $validated['email'],
+            'password' => Hash::make($validated['password']),
+        ]);
+
+        return response()->json(['user' => $user], 201);
+    }
+
+    public function login(Request $request)
+{
+    $validated = $request->validate([
+        'email' => 'required|string|email',
+        'password' => 'required|string',
+    ]);
+
+    $user = User::where('email', $validated['email'])->first();
+
+    if (!$user || !Hash::check($validated['password'], $user->password)) {
+        return response()->json([
+            'message' => 'The provided credentials are incorrect.'
+        ], 401); // Cambiar a 401 Unauthorized
+    }
+
+    $token = $user->createToken('auth_token')->plainTextToken;
+
+    return response()->json(['accessToken' => $token], 200);
+}
+
+}
