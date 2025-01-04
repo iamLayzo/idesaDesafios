17e23cce31bb9bde9acafcece1d13c93cd12dc9f
jesusb <jesusbclock@gmail.com>
2025-01-04 19:52:10 -0300

    fix: corregir lógica en retriveLotes para devolver todos los registros del lote en DesafioDos

--- desafioIDESA/DesafioDos.php
+++ desafioIDESA/DesafioDos.php
@@ -1,21 +1,19 @@
-<?php 
+<?php
 require_once 'Database.php';
 
 class DesafioDos {
-
-    public static function retriveLotes(int $loteID):void {
-
-        Database::setDB(); 
-
-        echo(json_encode(self::getLotes($loteID)));
+    public static function retriveLotes(string $loteID): void {
+        Database::setDB();
+        $lotes = self::getLotes($loteID);
+        echo json_encode($lotes, JSON_PRETTY_PRINT);
     }
 
-    private static function getLotes (int $loteID){
+    private static function getLotes(string $loteID): array {
         $lotes = [];
         $cnx = Database::getConnection();
-        $stmt = $cnx->query("SELECT * FROM debts WHERE lote = '$loteID' LIMIT 2");
-        while($rows = $stmt->fetchArray(SQLITE3_ASSOC)){
-            $lotes[] = (object) $rows;
+        $stmt = $cnx->query("SELECT * FROM debts WHERE lote = '$loteID'");
+        while ($rows = $stmt->fetchArray(SQLITE3_ASSOC)) {
+            $lotes[] = $rows;
         }
         return $lotes;
     }
